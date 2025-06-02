786dba01148593956b7d1901299f003b1de66f16
Alex <147999158+AklisTib@users.noreply.github.com>
2025-06-02 19:10:27 +0400

    Update HQ-SRV.sh

--- HQ-SRV.sh
+++ HQ-SRV.sh
@@ -41,6 +41,8 @@ awk -i inplace '/^#?MaxAuthTries[[:space:]]+6$/ {sub(/^#/,""); sub(/6/,"2"); pri
 awk -i inplace '/^#?PasswordAuthentication[[:space:]]+(yes|no)$/ {sub(/^#/,""); sub(/no/,"yes"); print; next} {print}' "$CONFIG_FILE"
 awk -i inplace '/^#?PubkeyAuthentication[[:space:]]+(yes|no)$/ {sub(/^#/,""); sub(/no/,"yes"); print; next} {print}' "$CONFIG_FILE"
 
+echo "Banner /etc/openssh/banner" | tee -a /etc/openssh/sshd_config
+
 touch /etc/openssh/banner  
 cat <<EOF > /etc/openssh/banner 
 
