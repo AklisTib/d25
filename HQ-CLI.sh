003f5ffe920b9d85cf7bf648b26927234da2f02f
Alex <147999158+AklisTib@users.noreply.github.com>
2025-06-02 16:33:49 +0400

    Update HQ-CLI.sh

--- HQ-CLI.sh
+++ HQ-CLI.sh
@@ -26,7 +26,7 @@ EOF
 systemctl restart sshd  
 
 echo "default_realm = AU-TEAM.IRPO" | sudo tee -a /etc/krb5.conf
-echo "nameserver 192.168.0.30" | sudo tee -a /etc/resolv.conf
+echo "nameserver 192.168.22.30" | sudo tee -a /etc/resolv.conf
 
 apt-get update && apt-get install -y gpupdate
 gpupdate-setup enable
